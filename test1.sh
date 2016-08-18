f3db16a28e0b693dddf3900078b62e6b7b02aab5
tom westberg <twestberg@solanolabs.com>
2016-08-18 17:50:26 -0400

    show exit code and fail

--- test1.sh
+++ test1.sh
@@ -3,8 +3,8 @@ sleep 2
 echo  "Did Test1" > test1.txt
 cat test1.txt
 ls -l
-printenv
-ls -l config
+# printenv
+# ls -l config
 # if [ -f config/database.yml ] 
 # 	then
 # 	echo "config/database.yml"
@@ -24,6 +24,6 @@ if [ -d $MYSQL_HOME ] ; then
 fi
 mysql --socket=$MYSQL_UNIX_PORT --user=$TDDIUM_DB_USER --password=$TDDIUM_DB_PASSWORD --execute="SHOW DATABASES;"  $TDDIUM_DB_NAME
 echo "trying through network"
-mysql --port=$TDDIUM_DB_PORT --host=$TDDIUM_DB_MYSQL_HOST --user=$TDDIUM_DB_USER --password=$TDDIUM_DB_PASSWORD --execute="SHOW DATABASES;" $TDDIUM_DB_NAME
+mysql --port=$TDDIUM_DB_PORT --host=$$TDDIUM_DB_MYSQL_HOST --user=$TDDIUM_DB_USER --password=$TDDIUM_DB_PASSWORD --execute="SHOW DATABASES;" $TDDIUM_DB_NAME
 echo "exit code was $?"
 exit 0
