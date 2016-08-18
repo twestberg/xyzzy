e8a291891fd6b820550b33741cf7bbb54c8d307e
tom westberg <twestberg@solanolabs.com>
2016-08-18 17:29:00 -0400

    try through net as well

--- test1.sh
+++ test1.sh
@@ -22,5 +22,8 @@ if [ -d $MYSQL_HOME ] ; then
 	ls $MYSQL_HOME
 	cat $MYSQL_HOME/*
 fi
-mysql --socket=$MYSQL_UNIX_PORT --user=$$TDDIUM_DB_MYSQL_NAME --password=$TDDIUM_DB_PASSWORD --execute="SHOW TABLES;" $TDDIUM_DB_NAME
+mysql --socket=$MYSQL_UNIX_PORT --user=$TDDIUM_DB_USER --password=$TDDIUM_DB_PASSWORD --execute="SHOW TABLES;" $TDDIUM_DB_NAME
+echo "trying through network"
+mysql --port=$TDDIUM_DB_PORT --host=$TDDIUM_DB_MSQL_HOST --user=$$TDDIUM_DB_USER --password=$TDDIUM_DB_PASSWORD --execute="SHOW TABLES;" $TDDIUM_DB_NAME
+
 exit 0
